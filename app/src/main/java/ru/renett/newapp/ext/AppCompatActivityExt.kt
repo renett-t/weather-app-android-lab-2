d686d95e881592b3674aece5ae14e86cfee76976
renett_t <rtapkina@gmail.com>
2022-02-26 12:34:37 +0300

    Added network related libraries, navigation component, detekt

---
+++ app/src/main/java/ru/renett/newapp/ext/AppCompatActivityExt.kt
@@ -0,0 +1,9 @@
+package ru.renett.newapp.ext
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+
+fun AppCompatActivity.findNavController(id: Int) : NavController {
+    return (supportFragmentManager.findFragmentById(id) as NavHostFragment).navController
+}
